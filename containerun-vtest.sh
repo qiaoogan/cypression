4850b19a232e4baa8c348216d4170571eae9a430
qiaoogan <qiaogang1984@163.com>
2024-03-18 10:28:34 +0800

    add vtest

--- containerun-vtest.sh
+++ containerun-vtest.sh
@@ -7,8 +7,6 @@ docker container run --rm \
   --add-host hw.piggy.instance:119.3.156.32 \
   --user 0  \
   --mount type=bind,source="$(pwd)",target=/home/cypression \
-  -e testLevel="smoke" \
-  -e testEnv="cloud" \
   -e VTEST_BUILD_VERSION="$BUILD_NUMBER" \
+  --entrypoint node /home/cypression/vtest.js \
   $image \
-  node /home/cypression/vtest.js
